c27324fe7409524caf7ba747437df97156f130c2
astutz96 <astutz96.development@protonmail.com>
2021-04-27 20:31:11 -0400

    fixing index and creating subfolders for components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Header from '../src/components/Header/Header';
+import Menu from '../src/components/Menu/Menu';
+import SpeakerSearchBar from '../src/components/SpeakerSearchBar/SpeakerSearchBar';
+import Speakers from '../src/components/Speakers/Speakers';
+import Footer from '../src/components/Footer/Footer';
+
 function Page() {
     return (
         <div>
-            <h1>Hello</h1>
+            <Header />
+            <Menu />
+            <SpeakerSearchBar />
+            <Speakers />
+            <Footer />
         </div>
     );
 }
--- src/components/SpeakerSearchBar.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'; 
-
-const SpeakerSearchBar = () => <img src='images/searchbar.png' />;
-
-export default SpeakerSearchBar;
